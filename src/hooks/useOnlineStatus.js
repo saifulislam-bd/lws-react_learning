d4ab627a41808a600c8ae5c9248be6609adba639
saifulislam-bd <saifulislam.dev22@gmail.com>
2025-05-30 22:26:14 +0600

    useOnlineStatus custom hook

---
+++ src/hooks/useOnlineStatus.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+
+    //synchronization
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    //cleanup
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+  return isOnline;
+}
